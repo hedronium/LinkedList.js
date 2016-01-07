d5a4d7947deb2b5d334d76b15b7391bcb32f578f
Omran Jamal <o.jamal97@gmail.com>
2016-01-07 16:51:10 +0600

    Dammit.

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-(function (module, define, window) {
-	var ns = {
-		List: require('./src/List'),
-		Node: require('./src/Node')
-	};
+var ns = {
+	List: require('./src/List'),
+	Node: require('./src/Node'),
+};
 
-	if (typeof module !== 'undefined') {
-		module.exports = ns;
-	} else if (typeof define !== 'undefined') {
-		define('LinkedListJS', function () {
-			return ns;
-		});
-	} else if (typeof window !== 'undefined') {
-		window.LinkedListJS = ns;
-	}
-})(module, define, window);
+if (typeof module !== 'undefined') {
+	module.exports = ns;
+} else if (typeof define !== 'undefined') {
+	define('LinkedListJS', function () {
+		return ns;
+	});
+} else if (typeof window !== 'undefined') {
+	window.LinkedListJS = ns;
+}
